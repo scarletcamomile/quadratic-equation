999d3dad7ae587bd76edb88b7e1e8fc6a2a88f73
sugar-plum fairy <scarletcamomile@mail.ru>
2018-09-23 13:16:16 +0300

    quick sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  let helpArr = [];
+  equation.split(' ').forEach((el, i, arr) => {
+    if (el === 'x^2') helpArr = [...helpArr, arr[i - 2]];
+    else if (el === 'x') helpArr = [...helpArr, arr[i - 3] + arr[i - 2], arr[i + 1] + arr[i + 2]];
+  });
+  const D = sqrt(Math.pow(helpArr[1], 2) - 4 * helpArr[0] * helpArr[2]);
+  return [
+    Math.round((-helpArr[1] + D) / (2 * helpArr[0])),
+    Math.round((-helpArr[1] - D) / (2 * helpArr[0]))
+  ].sort((a, b) => a - b);
+};
+
+const sqrt = num => {
+  let iter = 1;
+  for (;;) {
+    let help = (iter + num / iter) / 2;
+    if (Math.abs(iter - help) < 1) break;
+    iter = help;
+  }
+  return iter;
+};
